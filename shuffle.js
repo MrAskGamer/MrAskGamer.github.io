95b9296f9feecc62472fd6770f9a6432d29de93f
MrAskGamer <80630840+MrAskGamer@users.noreply.github.com>
2023-10-19 07:59:37 +0400

    Add files via upload

---
+++ shuffle.js
@@ -0,0 +1,18 @@
+function shuffle(array) {let currentIndex = array.length,  randomIndex;while (currentIndex != 0) {randomIndex = Math.floor(Math.random() * currentIndex);currentIndex--;[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];}return array;}
+
+document.querySelectorAll(".correct").forEach(e => e.addEventListener('click', () => {e.style.background = '#90ee90'}))
+document.querySelectorAll(".wrong").forEach(e => e.addEventListener('click', () => {e.style.background = '#FFCCCB'}))
+
+document.addEventListener('keypress', (e) => {
+    if (e.key == 'r') {
+        document.querySelectorAll(".correct, .wrong").forEach(e => e.style.background = '')
+        let temp = shuffle(Array.prototype.slice.call(document.body.children)).filter(e => e.tagName == 'DIV')
+        temp.forEach(e => {
+            let temp1 = shuffle(Array.prototype.slice.call(e.children[1].children))
+            e.children[1].innerHTML = ''
+            temp1.forEach(j => e.children[1].appendChild(j))
+        })
+        document.body.innerHTML = ''
+        temp.forEach(e => {document.body.appendChild(e)})
+    }
+})
